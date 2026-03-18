bda326e93d68ccc6891b5cee118a836fbf997c71
ozarhu2049 <94573452+ozarhu2049@users.noreply.github.com>
2026-03-18 17:11:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,7 @@
-from fastapi import FastAPI, HTTPException
-from manager import start_bot, stop_bot
+from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"status": "ok"}
-
-@app.post("/start-bot")
-def api_start_bot(data: dict):
-    username = data.get("username")
-
-    if not username:
-        raise HTTPException(status_code=400, detail="Username required")
-
-    start_bot(username)
-
-    return {"message": f"Bot started for {username}"}
-
-@app.post("/stop-bot")
-def api_stop_bot(data: dict):
-    username = data.get("username")
-
-    if not username:
-        raise HTTPException(status_code=400, detail="Username required")
-
-    stop_bot(username)
-
-    return {"message": f"Bot stopped for {username}"}
